d4a1add1517d76b7b8417427dc9389936827c949
Дашноров дамдиндорж <23LP9512@G301-CC0132.local>
2024-01-26 12:18:24 +0800

    class

--- src/pages/clock.js
+++ src/pages/clock.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
+
 const Clock = () => {
-  
-  return <div></div>;
+  let time = new Date().toLocaleTimeString();
+  const [clock, setClock] = useState("");
+  function updateClock() {
+    let time = new Date().toLocaleTimeString();
+    setClock(time);
+  }
+  setInterval(updateClock, 1000);
+  return <div>{time}</div>;
 };
 
 export default Clock;
--- src/pages/stopwatch.js
+++ src/pages/stopwatch.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const Stopwatch = () => {
-  return <div>Stopwatch</div>;
+  let [ms, setMs] = useState("0");
+  let [ss, setSs] = useState("0");
+  let [mm, setMm] = useState("0");
+  let [hh, setHh] = useState("0");
+
+  let stopwatch = `${hh}` + ":" + `${mm}` + ":" + `${ss}` + ":" + `${ms}`;
+
+  return (
+    <div>
+      <div>{stopwatch}</div>
+      <button>start</button>
+      <button>stop</button>
+      <button>reset</button>
+    </div>
+  );
 };
 
 export default Stopwatch;
